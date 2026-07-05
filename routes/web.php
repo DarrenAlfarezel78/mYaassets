67015b258d5376b20e4450a1a8f100700b68edc7
DarrenAlfarezel78 <dddarren10310@gmail.com>
2026-07-05 14:50:21 +0700

    feat: membuat controller resource dan mendaftarkan route master data dengan proteksi role

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,28 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    
+    // Semua role bisa masuk ke Dashboard
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+    // Fitur Master Data Barang (HANYA boleh diakses oleh Admin & Staff)
+    Route::middleware(['role:Admin,Staff'])->group(function () {
+        Route::resource('categories', CategoryController::class);
+        Route::resource('products', ProductController::class);
+    });
+
+    // Profil Bawaan Breeze
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
